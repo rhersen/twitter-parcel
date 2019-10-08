8c719c534eb63bfb881f96f2c36fdebca681a6fe
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-10-08 16:45:38 +0200

    Add serverless function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log('hello world')
 
 document.getElementById('root').insertAdjacentHTML('beforeend', '<span>hello world</span>')
+
+async function f() {
+  let response = await fetch(`/.netlify/functions/fauna`)
+  console.log(await response.json());
+}
+
+f()
