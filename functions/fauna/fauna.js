95a5dfab24fcdae4e6415b6c4282d9ad121801b4
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-10-09 22:39:30 +0200

    Add prettier

--- functions/fauna/fauna.js
+++ functions/fauna/fauna.js
@@ -1,47 +1,47 @@
-/* eslint-disable */
-const faunadb = require('faunadb');
+const faunadb = require("faunadb")
 
-const q = faunadb.query;
+const q = faunadb.query
 
 const faunaClient = new faunadb.Client({
-  secret: process.env.FAUNADB_SECRET,
-});
+  secret: process.env.FAUNADB_SECRET
+})
 
 async function lastRead() {
   try {
-    const ret = await faunaClient.query(q.Get(q.Match(q.Index('all_last_read'))));
-    console.log(ret);
-    return ret;
+    const ret = await faunaClient.query(
+      q.Get(q.Match(q.Index("all_last_read")))
+    )
+    console.log(ret)
+    return ret
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
 }
 
 async function mark(ref, id_str) {
   try {
-    await faunaClient.query(q.Replace(ref, {data: {id_str}}));
+    await faunaClient.query(q.Replace(ref, { data: { id_str } }))
   } catch (e) {
-    console.error(e);
+    console.error(e)
   }
 }
 
-exports.handler = async function (event, context) {
+exports.handler = async function(event, context) {
   try {
-    const response = await lastRead();
-    console.log(response);
-    if (!response.data) {
-      // NOT res.status >= 200 && res.status < 300
-      return {statusCode: response.requestResult.statusCode};
-    }
+    const response = await lastRead()
+    console.log(response)
+    if (!response.data) return { statusCode: response.requestResult.statusCode }
 
     return {
-      statusCode: 200, body: JSON.stringify(response.data)
-    };
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    }
   } catch (err) {
-    console.log(err); // output to netlify function log
+    console.log(err) // output to netlify function log
     return {
-      statusCode: 500, body: JSON.stringify({msg: err.message}) // Could be a custom message or object i.e.
-                                                                // JSON.stringify(err)
-    };
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e.
+      // JSON.stringify(err)
+    }
   }
-};
+}
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-(async function () {
+iife().then(() => {
+  console.log("done")
+})
+
+async function iife() {
   const faunaResp = await fetch(`/.netlify/functions/fauna`)
-  const faunaJson = await faunaResp.json()
-  const tweetResp = await fetch(`/.netlify/functions/twitter?since_id=${faunaJson.id_str}`)
-  const tweetJson = await tweetResp.json()
-  tweetJson.forEach(tweet => document.getElementById('root').insertAdjacentHTML('beforeend', `<div>${tweet.full_text}</div>`))
-})()
+  const tweetResp = await fetch(
+    `/.netlify/functions/twitter?since_id=${(await faunaResp.json()).id_str}`
+  )
+
+  ;(await tweetResp.json()).forEach(tweet =>
+    document
+      .getElementById("root")
+      .insertAdjacentHTML("beforeend", `<hr /><div>${tweet.full_text}</div>`)
+  )
+}
