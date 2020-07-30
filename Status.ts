bdfcbf011b3f9b112f811390464cf361b1a667fc
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-07-30 21:55:10 +0200

    convert renderTweet to typescript

---
+++ Status.ts
@@ -0,0 +1,253 @@
+export interface Hashtag {
+  text: string
+  indices: number[]
+}
+
+export interface UserMention {
+  screen_name: string
+  name: string
+  id: any
+  id_str: string
+  indices: number[]
+}
+
+export interface Url {
+  url: string
+  expanded_url: string
+  display_url: string
+  indices: number[]
+}
+
+export interface Size {
+  w: number
+  h: number
+  resize: string
+}
+
+export interface Sizes {
+  large: Size
+  medium: Size
+  small: Size
+  thumb: Size
+}
+
+export interface Variant {
+  bitrate: number
+  content_type: string
+  url: string
+}
+
+export interface VideoInfo {
+  aspect_ratio: number[]
+  duration_millis: number
+  variants: Variant[]
+}
+
+export interface AdditionalMediaInfo {
+  monetizable: boolean
+}
+
+export interface Medium {
+  id: any
+  id_str: string
+  indices: number[]
+  media_url: string
+  media_url_https: string
+  url: string
+  display_url: string
+  expanded_url: string
+  type: string
+  sizes: Sizes
+  source_status_id: any
+  source_status_id_str: string
+  source_user_id: number
+  source_user_id_str: string
+  video_info: VideoInfo
+  additional_media_info: AdditionalMediaInfo
+}
+
+export interface ExtendedEntities {
+  media: Medium[]
+}
+
+export interface QuotedStatusPermalink {
+  url: string
+  expanded: string
+  display: string
+}
+
+export interface User {
+  id: number
+  id_str: string
+  name: string
+  screen_name: string
+  location: string
+  description: string
+  url: string
+  entities: Entities
+  protected: boolean
+  followers_count: number
+  friends_count: number
+  listed_count: number
+  created_at: string
+  favourites_count: number
+  utc_offset?: any
+  time_zone?: any
+  geo_enabled: boolean
+  verified: boolean
+  statuses_count: number
+  lang?: any
+  contributors_enabled: boolean
+  is_translator: boolean
+  is_translation_enabled: boolean
+  profile_background_color: string
+  profile_background_image_url: string
+  profile_background_image_url_https: string
+  profile_background_tile: boolean
+  profile_image_url: string
+  profile_image_url_https: string
+  profile_banner_url: string
+  profile_link_color: string
+  profile_sidebar_border_color: string
+  profile_sidebar_fill_color: string
+  profile_text_color: string
+  profile_use_background_image: boolean
+  has_extended_profile: boolean
+  default_profile: boolean
+  default_profile_image: boolean
+  following: boolean
+  follow_request_sent: boolean
+  notifications: boolean
+  translator_type: string
+}
+
+export interface ExtendedEntities {
+  media: Medium[]
+}
+
+export interface RetweetedStatus {
+  created_at: string
+  id: any
+  id_str: string
+  full_text: string
+  truncated: boolean
+  display_text_range: number[]
+  entities: Entities
+  extended_entities: ExtendedEntities
+  source: string
+  in_reply_to_status_id?: number
+  in_reply_to_status_id_str: string
+  in_reply_to_user_id?: number
+  in_reply_to_user_id_str: string
+  in_reply_to_screen_name: string
+  user: User
+  geo?: any
+  coordinates?: any
+  place?: any
+  contributors?: any
+  is_quote_status: boolean
+  retweet_count: number
+  favorite_count: number
+  favorited: boolean
+  retweeted: boolean
+  possibly_sensitive: boolean
+  possibly_sensitive_appealable: boolean
+  lang: string
+  quoted_status_id?: number
+  quoted_status_id_str: string
+  quoted_status_permalink: QuotedStatusPermalink
+  quoted_status: QuotedStatus
+}
+
+export interface QuotedStatusPermalink2 {
+  url: string
+  expanded: string
+  display: string
+}
+
+export interface Symbol {
+  text: string
+  indices: number[]
+}
+
+export interface Entities {
+  hashtags: Hashtag[]
+  symbols: Symbol[]
+  user_mentions: UserMention[]
+  urls: Url[]
+  media: Medium[]
+}
+
+export interface QuotedStatusPermalink3 {
+  url: string
+  expanded: string
+  display: string
+}
+
+export interface QuotedStatus {
+  created_at: string
+  id: any
+  id_str: string
+  full_text: string
+  truncated: boolean
+  display_text_range: number[]
+  entities: Entities
+  extended_entities: ExtendedEntities
+  source: string
+  in_reply_to_status_id?: number
+  in_reply_to_status_id_str: string
+  in_reply_to_user_id?: number
+  in_reply_to_user_id_str: string
+  in_reply_to_screen_name: string
+  user: User
+  geo?: any
+  coordinates?: any
+  place?: any
+  contributors?: any
+  is_quote_status: boolean
+  retweet_count: number
+  favorite_count: number
+  favorited: boolean
+  retweeted: boolean
+  possibly_sensitive: boolean
+  possibly_sensitive_appealable: boolean
+  lang: string
+  quoted_status_id?: number
+  quoted_status_id_str: string
+  quoted_status_permalink: QuotedStatusPermalink3
+}
+
+export interface Status {
+  created_at: string
+  id: any
+  id_str: string
+  full_text: string
+  truncated: boolean
+  display_text_range: number[]
+  entities: Entities
+  extended_entities: ExtendedEntities
+  source: string
+  in_reply_to_status_id?: any
+  in_reply_to_status_id_str?: any
+  in_reply_to_user_id?: any
+  in_reply_to_user_id_str?: any
+  in_reply_to_screen_name?: any
+  user: User
+  geo?: any
+  coordinates?: any
+  place?: any
+  contributors?: any
+  retweeted_status: RetweetedStatus
+  is_quote_status: boolean
+  retweet_count: number
+  favorite_count: number
+  favorited: boolean
+  retweeted: boolean
+  possibly_sensitive: boolean
+  possibly_sensitive_appealable: boolean
+  lang: string
+  quoted_status_id?: number
+  quoted_status_id_str: string
+  quoted_status_permalink: QuotedStatusPermalink2
+  quoted_status: QuotedStatus
+}
--- renderTweet.ts
+++ renderTweet.ts
@@ -1,20 +1,31 @@
-export default function renderTweet(tweet) {
-  const retweet = tweet.retweeted_status
+import {
+  Medium,
+  RetweetedStatus,
+  Status,
+  Url,
+  Variant,
+  VideoInfo
+} from "./Status"
 
-  const time = tweet.created_at ? tweet.created_at.substr(8, 8) : "When?"
-  const user = getUser(retweet, tweet)
-  const image = getImages(retweet || tweet)
+export default function renderTweet(tweet: Status) {
+  const retweet: RetweetedStatus = tweet.retweeted_status
 
-  const a = `<a onclick='mark("${tweet.id_str}")'>${time}</a>`
-  const i = getRetweeter(retweet, tweet)
-  const b = `<b>${user}</b>`
-  const text = getText(retweet, tweet)
-  const images = image && `<div>${image}</div>`
-  const quote = getQuote(retweet || tweet)
+  const time: string = tweet.created_at
+    ? tweet.created_at.substr(8, 8)
+    : "When?"
+  const user: string = getUser(retweet, tweet)
+  const image: string = getImages(retweet || tweet)
+
+  const a: string = `<a onclick='mark("${tweet.id_str}")'>${time}</a>`
+  const i: string = getRetweeter(retweet, tweet)
+  const b: string = `<b>${user}</b>`
+  const text: string = getText(retweet, tweet)
+  const images: string = image && `<div>${image}</div>`
+  const quote: string = getQuote(retweet || tweet)
 
   return `<li>${a}${i}${b} ${text} ${quote} ${images}</li>`
 
-  function getImages(d) {
+  function getImages(d: RetweetedStatus | Status): string {
     if (!d.extended_entities || !d.extended_entities.media) return ""
 
     return d.extended_entities.media
@@ -22,7 +33,7 @@ export default function renderTweet(tweet) {
       .map(getImage)
       .join("")
 
-    function isPhoto(img) {
+    function isPhoto(img: Medium) {
       return (
         img.type === "photo" ||
         img.type === "video" ||
@@ -30,7 +41,7 @@ export default function renderTweet(tweet) {
       )
     }
 
-    function getImage(image) {
+    function getImage(image: Medium): string {
       const size = image.sizes.small
       const width = size.w / 2
       const height = size.h / 2
@@ -42,7 +53,7 @@ export default function renderTweet(tweet) {
       return `<a href="${large}">${img}</a>${duration}`
     }
 
-    function getVideoLink(info, imageType) {
+    function getVideoLink(info: VideoInfo, imageType: string): string {
       if (!info || !info.variants || !info.variants.length) return ""
       const best = info.variants.reduce(maxBitrate)
       const duration = info.duration_millis
@@ -51,17 +62,17 @@ export default function renderTweet(tweet) {
       return `<a href="${best.url}">${duration}</a>`
     }
 
-    function maxBitrate(prev, cur) {
+    function maxBitrate(prev: Variant, cur: Variant): Variant {
       return bitrate(cur) > bitrate(prev) ? cur : prev
     }
 
-    function bitrate(variant) {
+    function bitrate(variant: Variant): number {
       return variant.bitrate || 0
     }
   }
 }
 
-function getUser(retweet, d) {
+function getUser(retweet: RetweetedStatus, d: Status): string {
   return retweet && retweet.user
     ? retweet.user.screen_name
     : d.user
@@ -69,20 +80,20 @@ function getUser(retweet, d) {
     : "Who?"
 }
 
-function getRetweeter(retweet, d) {
+function getRetweeter(retweet: RetweetedStatus, d: Status): string {
   return retweet && d.user && d.user.screen_name
     ? ` <i>${d.user.screen_name}</i> `
     : " "
 }
 
-function getText(retweetStatus, tweetStatus) {
+function getText(retweetStatus: RetweetedStatus, tweetStatus: Status): string {
   const data = retweetStatus || tweetStatus
 
   return data.entities
     ? data.entities.urls.reduce(replaceUrlWithLink, fullText(data))
     : fullText(data)
 
-  function replaceUrlWithLink(text, url) {
+  function replaceUrlWithLink(text: string, url: Url): string {
     return text.replace(
       url.url,
       `<a href="${url.url}" target="_blank">${url.display_url || url.url}</a>`
@@ -90,12 +101,12 @@ function getText(retweetStatus, tweetStatus) {
   }
 }
 
-function getQuote(d) {
+function getQuote(d: RetweetedStatus | Status): string {
   return d.quoted_status
     ? `<div class="quoted">${fullText(d.quoted_status)}</div>`
     : ""
 }
 
-function fullText(data) {
+function fullText(data): string {
   return data.full_text && data.full_text.replace(/\n/g, "<br>")
 }
