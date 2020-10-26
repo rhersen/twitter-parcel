9d4c048ccf3345c29b10e9fcaafe47e3696a5976
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-10-26 22:53:54 +0100

    promisify

--- fetchAndShowTweets.js
+++ fetchAndShowTweets.js
@@ -1,45 +1,48 @@
 import { getUsers } from "./Users.bs.js"
 import { renderTweet } from "./Tweet.bs.js"
 
-export async function fetchAndShowTweets(id_str, tweets) {
-  const tweetResp = await fetch(
-    `/.netlify/functions/twitter?since_id=${id_str}`
-  )
+export function fetchAndShowTweets(id_str, tweets) {
+  fetch(`/.netlify/functions/twitter?since_id=${id_str}`).then(tweetResp => {
+    if (tweetResp.ok) {
+      setStatus("insertAdjacentHTML")
+      tweetResp.json().then(tweetJson => handleResponse(tweetJson, tweets))
+    } else {
+      tweetResp.text().then(s => setStatus(`twitter GET error: ${s}`))
+    }
+  })
+}
 
-  if (tweetResp.ok) {
-    setStatus("insertAdjacentHTML")
-    const tweetJson = await tweetResp.json()
-    const users = getUsers(tweetJson)
+function handleResponse(tweetJson, tweets) {
+  const users = getUsers(tweetJson)
 
-    let i = 0
-    tweetJson.forEach(tweet => {
-      tweets.insertAdjacentHTML("afterbegin", renderTweet(tweet))
-      tweets.insertAdjacentHTML(
-        "afterbegin",
-        `<div class="stats"><span class="countdown" onclick='mark("${
-          tweet.id_str
-        }")'>${++i}</span><hr /></div>`
-      )
-    })
-
+  let i = 0
+  tweetJson.forEach(tweet => {
+    tweets.insertAdjacentHTML("afterbegin", renderTweet(tweet))
     tweets.insertAdjacentHTML(
       "afterbegin",
-      `<table>${Object.keys(users)
-        .map(key => {
-          if (users[key] > 4)
-            return `<tr><td>${key}</td><td>${users[key]}</td></tr>`
-        })
-        .join("")}</table>`
+      `<div class="stats"><span class="countdown" onclick='mark("${
+        tweet.id_str
+      }")'>${++i}</span><hr /></div>`
     )
+  })
 
-    setStatus("addEventListener")
-    tweets.querySelectorAll("a.mark").forEach(a => {
-      a.addEventListener("click", window.mark)
-    })
-    setStatus("twitter GET OK")
-  } else setStatus(`twitter GET error: ${await tweetResp.text()}`)
+  tweets.insertAdjacentHTML(
+    "afterbegin",
+    `<table>${Object.keys(users)
+      .map(key => {
+        if (users[key] > 4)
+          return `<tr><td>${key}</td><td>${users[key]}</td></tr>`
+      })
+      .join("")}</table>`
+  )
 
-  function setStatus(s) {
-    document.getElementById("status").innerHTML = s
-  }
+  setStatus("addEventListener")
+  tweets.querySelectorAll("a.mark").forEach(a => {
+    a.addEventListener("click", window.mark)
+  })
+  setStatus("twitter GET OK")
+}
+
+function setStatus(s) {
+  document.getElementById("status").innerHTML = s
 }
