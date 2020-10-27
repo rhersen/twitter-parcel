ff5f0b923692f96c3a9f9de98b72f92e44b67c16
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-10-27 21:54:31 +0100

    prepare fetchAndShowTweets.js for res

--- fetchAndShowTweets.js
+++ fetchAndShowTweets.js
@@ -1,48 +1,60 @@
 import { getUsers } from "./Users.bs.js"
 import { renderTweet } from "./Tweet.bs.js"
 
-export function fetchAndShowTweets(id_str, tweets) {
-  fetch(`/.netlify/functions/twitter?since_id=${id_str}`).then(tweetResp => {
-    if (tweetResp.ok) {
-      setStatus("insertAdjacentHTML")
-      tweetResp.json().then(tweetJson => handleResponse(tweetJson, tweets))
-    } else {
-      tweetResp.text().then(s => setStatus(`twitter GET error: ${s}`))
-    }
-  })
+let setStatus = s => {
+  document.getElementById("status").innerHTML = s
 }
 
-function handleResponse(tweetJson, tweets) {
-  const users = getUsers(tweetJson)
+let addEventListener = a => {
+  a.addEventListener("click", window.mark)
+}
 
-  let i = 0
-  tweetJson.forEach(tweet => {
-    tweets.insertAdjacentHTML("afterbegin", renderTweet(tweet))
+let setErrorStatus = s => setStatus(`twitter GET error: ${s}`)
+
+export let fetchAndShowTweets = (id_str, tweets) => {
+  let handleJson = tweetJson => {
+    let users = getUsers(tweetJson)
+    let i = 0
+
+    let renderTweets = tweet => {
+      tweets.insertAdjacentHTML("afterbegin", renderTweet(tweet))
+      tweets.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="stats"><span class="countdown" onclick='mark("${
+          tweet.id_str
+        }")'>${++i}</span><hr /></div>`
+      )
+    }
+
+    let insertUsers = key => {
+      if (users[key] > 4) {
+        return `<tr><td>${key}</td><td>${users[key]}</td></tr>`
+      }
+    }
+
+    tweetJson.forEach(renderTweets)
+
     tweets.insertAdjacentHTML(
       "afterbegin",
-      `<div class="stats"><span class="countdown" onclick='mark("${
-        tweet.id_str
-      }")'>${++i}</span><hr /></div>`
+      `<table>${Object.keys(users)
+        .map(insertUsers)
+        .join("")}</table>`
     )
-  })
 
-  tweets.insertAdjacentHTML(
-    "afterbegin",
-    `<table>${Object.keys(users)
-      .map(key => {
-        if (users[key] > 4)
-          return `<tr><td>${key}</td><td>${users[key]}</td></tr>`
-      })
-      .join("")}</table>`
-  )
+    setStatus("addEventListener")
+    tweets.querySelectorAll("a.mark").forEach(addEventListener)
+    setStatus("twitter GET OK")
+  }
 
-  setStatus("addEventListener")
-  tweets.querySelectorAll("a.mark").forEach(a => {
-    a.addEventListener("click", window.mark)
-  })
-  setStatus("twitter GET OK")
-}
+  let handleFetch = tweetResp => {
+    if (tweetResp.ok) {
+      setStatus("insertAdjacentHTML")
 
-function setStatus(s) {
-  document.getElementById("status").innerHTML = s
+      tweetResp.json().then(handleJson)
+    } else {
+      tweetResp.text().then(setErrorStatus)
+    }
+  }
+
+  fetch(`/.netlify/functions/twitter?since_id=${id_str}`).then(handleFetch)
 }
