a5f83894aec5523e3cf012844e15e622f1562485
Rudolf Hersén <rudolf.hersen@schibsted.com>
2020-11-04 21:50:41 +0100

    prepare index.js for conversion

--- index.js
+++ index.js
@@ -2,24 +2,35 @@ import { fetchAndShowTweets } from "./Tweets.bs.js"
 import { set as setStatus } from "./Status.bs.js"
 import { mark } from "./Mark.bs.js"
 
+let handleFaunaResponse = faunaResp => {
+  if (faunaResp.ok) {
+    faunaResp.json().then(twitterGet)
+  } else {
+    faunaResp.text().then(faunaGetError)
+  }
+}
+
+let faunaGetError = text => {
+  setStatus("fauna GET error: " + text)
+}
+
+let twitterGet = ({ id_str }) => {
+  setStatus("twitter GET")
+  fetchAndShowTweets(id_str, document.getElementById("tweets")).then(
+    logDone,
+    logFail
+  )
+}
+
+let logDone = () => {
+  console.log("done")
+}
+
+let logFail = () => {
+  console.log("fail")
+}
+
 setStatus("fauna GET")
-fetch(`/.netlify/functions/fauna`).then(faunaResp => {
-  if (!faunaResp.ok) {
-    faunaResp.text().then(text => {
-      setStatus(`fauna GET error: ${text}`)
-    })
-  } else
-    faunaResp.json().then(({ id_str }) => {
-      setStatus("twitter GET")
-      fetchAndShowTweets(id_str, document.getElementById("tweets")).then(
-        () => {
-          console.log("done")
-        },
-        () => {
-          console.log("fail")
-        }
-      )
-    })
-})
+fetch("/.netlify/functions/fauna").then(handleFaunaResponse)
 
 window.mark = mark
